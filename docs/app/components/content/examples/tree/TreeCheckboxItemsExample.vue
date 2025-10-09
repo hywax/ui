235387ee62e92cdf6a4a888af4f9aee0eee2f7cc
hywax <a.hywax@gmail.com>
2025-10-09 18:04:34 +0500

    feat(Tree): add example for tree component with checkbox items

---
+++ docs/app/components/content/examples/tree/TreeCheckboxItemsExample.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import type { TreeItem } from '@nuxt/ui'
+import type { TreeItemSelectEvent, TreeItemToggleEvent } from 'reka-ui'
+
+function onSelect(event: TreeItemSelectEvent<TreeItem>) {
+  if (event.detail.originalEvent.type === 'click') {
+    event.preventDefault()
+  }
+}
+
+function onToggle(event: TreeItemToggleEvent<TreeItem>) {
+  if (event.detail.originalEvent.type === 'keydown') {
+    event.preventDefault()
+  }
+}
+
+const selectedItems = ref<(typeof items)>([])
+const items = [
+  {
+    label: 'app/',
+    onSelect,
+    onToggle,
+    children: [
+      {
+        label: 'composables/',
+        onSelect,
+        onToggle,
+        children: [
+          { label: 'useAuth.ts' },
+          { label: 'useUser.ts' }
+        ]
+      },
+      {
+        label: 'components/',
+        defaultExpanded: true,
+        onSelect,
+        onToggle,
+        children: [
+          { label: 'Card.vue' },
+          { label: 'Button.vue' }
+        ]
+      }
+    ]
+  },
+  { label: 'app.vue' },
+  { label: 'nuxt.config.ts' }
+] satisfies TreeItem[]
+</script>
+
+<template>
+  <UTree
+    v-model="selectedItems"
+    :items="items"
+    multiple
+    propagate-select
+    bubble-select
+  >
+    <template #item-leading="{ selected, indeterminate, handleSelect }">
+      <UCheckbox
+        :model-value="indeterminate ? 'indeterminate' : selected"
+        data-tree-select-checkbox="true"
+        @change="handleSelect"
+        @click.stop
+      />
+    </template>
+  </UTree>
+</template>
